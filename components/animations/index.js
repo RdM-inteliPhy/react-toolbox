f16695941c176bb3ee9ee62e7756b8da0bbb067b
Nenad <nrakic90@live.com>
2017-07-17 10:17:31 +0200

    test commit

--- components/animations/index.js
+++ components/animations/index.js
@@ -2,3 +2,5 @@ export SlideLeft from './slide-left.scss';
 export SlideRight from './slide-right.scss';
 export ZoomIn from './zoom-in.scss';
 export ZoomOut from './zoom-out.scss';
+
+// TEST COMMIT
